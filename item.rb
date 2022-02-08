2b1daf769b98aa538767dfff6368e421b873c9e6
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-08 10:57:27 +0200

    Fix Rubocop offenses

--- item.rb
+++ item.rb
@@ -1,14 +1,14 @@
 # create item class
 class Item
-    attr_accessor :publish_date, :genre, :author, :label
-    attr_reader :id
-  
-    def initialize(publish_date)
-      @id = Random.rand(1..10_000)
-      @genre = nil
-      @author = nil
-      @label = nil
-      @publish_date = publish_date
-      @archived = false
-    end
+  attr_accessor :publish_date, :genre, :author, :label
+  attr_reader :id
+
+  def initialize(publish_date)
+    @id = Random.rand(1..10_000)
+    @genre = nil
+    @author = nil
+    @label = nil
+    @publish_date = publish_date
+    @archived = false
+  end
 end
