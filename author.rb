e89f92d7b24c4559f03340f674729eb7b2f091e8
hamid-murambiwa <machipisajunior@gmail.com>
2022-02-08 18:46:32 +0200

    Add Author class with add_item method to the author.rb file

---
+++ author.rb
@@ -0,0 +1,19 @@
+require_relative './item'
+
+class Author < Item
+  attr_accessor :first_name, :last_name
+  attr_reader :items
+
+  def initialize(first_name, last_name)
+    super()
+    @id = Random.rand(1..10_000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(instance)
+    @items << instance
+    instance.author = self
+  end
+end
