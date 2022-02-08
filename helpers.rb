5fe3041d9d7a752671d63d7373d8badebb8f55ef
hamid-murambiwa <machipisajunior@gmail.com>
2022-02-08 12:42:22 +0200

    Fix linters errors

--- helpers.rb
+++ helpers.rb
@@ -1,28 +1,28 @@
 module Helpers
-    def continue?
-        print "\nDo you wish to continue? [Y/N]: "
-        answer = gets.chomp
-    
-        if answer.downcase == 'y' || answer.downcase == 'yes' || answer == ''
-          clear
-          prompt_user
-        else
-          exit
-        end
-      end
-    
-      def invalid_prompt
-        clear
-        puts 'Incorrect selection, please try again!'
-        sleep 1
-      end
-    
-      def clear
-        print "\e[2J\e[f"
-      end
-    
-      def exit
-        clear
-        nil
-      end
+  def continue?
+    print "\nDo you wish to continue? [Y/N]: "
+    answer = gets.chomp
+
+    if answer.downcase == 'y' || answer.downcase == 'yes' || answer == ''
+      clear
+      prompt_user
+    else
+      exit
+    end
+  end
+
+  def invalid_prompt
+    clear
+    puts 'Incorrect selection, please try again!'
+    sleep 1
+  end
+
+  def clear
+    print "\e[2J\e[f"
+  end
+
+  def exit
+    clear
+    nil
+  end
 end
--- main.rb
+++ main.rb
@@ -1,22 +1,34 @@
 require_relative 'helpers'
 
-#Create App class
+# Create App class
 class App
-    include Helpers
+  include Helpers
 
-    def run
-        puts "\nWelcome to the 'Catalog of my things' app"
-        prompt_user
+  def run
+    puts "\nWelcome to the 'Catalog of my things' app"
+    prompt_user
+  end
+
+  def prompt_user
+    options
+    input = gets.chomp
+
+    if input.to_i.between?(1, 8)
+      clear
+      list_selection(input)
+    elsif input.to_i.between?(9, 12)
+      clear
+      add_selection(input)
+    elsif input == '13'
+      exit
+    else
+      invalid_prompt
+      prompt_user
     end
+  end
 
-    def prompt_user
-        options
-        input = gets.chomp
-        selection(input)
-    end
-
-    def options
-        puts "\n Choose from the options below: 
+  def options
+    puts "\n Choose from the options below:
         1 - List all books
         2 - List all music albums
         3 - List all movies
@@ -31,44 +43,49 @@ class App
         12 - Add a game
         13 - exit
         "
-    end
+  end
 
-    def selection(input)
-        case input
-        when '1'
-            puts 'List all books'
-        when '2'
-            puts 'List all music albums'
-        when '3'
-            puts 'List all movies'
-        when '4'
-            puts 'List of games'
-        when '5'
-            puts 'List all genres'
-        when '6'
-            puts 'List all labels'
-        when '7'
-            puts 'List all authors'
-        when '8'
-            puts 'List all sources'
-        when '9'
-            puts 'Add a book'
-        when '10'
-            puts 'Add a music album'
-        when '11'
-            puts 'Add a movie'
-        when '12'
-            puts 'Add a game'
-        when '13'
-            puts 'Goob bye (:......'
-            exit
-        end
+  def list_selection(input)
+    case input
+    when '1'
+      puts 'List all books'
+    when '2'
+      puts 'List all music albums'
+    when '3'
+      puts 'List all movies'
+    when '4'
+      puts 'List of games'
+    when '5'
+      puts 'List all genres'
+    when '6'
+      puts 'List all labels'
+    when '7'
+      puts 'List all authors'
+    when '8'
+      puts 'List all sources'
     end
+  end
+
+  def add_selection(input)
+    case input
+    when '9'
+      puts 'Add a book'
+    when '10'
+      puts 'Add a music album'
+    when '11'
+      puts 'Add a movie'
+    when '12'
+      puts 'Add a game'
+    when '13'
+      puts 'Goob bye (:......'
+      exit
+    end
+  end
 end
 
 def load
-    start = App.new
-    start.run
+  start = App.new
+  start.run
 end
 
 load
