8607249da71771dcf048958b36a6b2386278d044
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-09 09:45:38 +0200

    Update ListItems class

--- list_items.rb
+++ list_items.rb
@@ -2,13 +2,14 @@ require './book'
 
 module ListItems
   def list_labels
+    puts 'List of all labels : '
     @labels.each_with_index { |label, index| puts "#{index}) Title: #{label.title} | Color: #{label.color}" }
   end
 
   def list_all_books
-    puts 'List of all Books : '
+    puts 'List of all books : '
     if @books.empty?
-      puts "\n Books list is empty."
+      puts "\nBooks list is empty."
     else
       @books.each_with_index do |book, index|
         puts "#{index}) Title: #{book.label.title} | Publisher: #{book.publisher} | Condition: #{book.cover_state}"
