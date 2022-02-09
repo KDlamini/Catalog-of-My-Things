ba84659be67834bf18ac938f747e8d481082b955
mbdesigns1989 <mbdesigns1989@yahoo.com>
2022-02-09 11:28:52 +0200

    Add genre class

---
+++ classes/genre.rb
@@ -0,0 +1,14 @@
+# require_relative './item'
+
+class Genre
+  attr_accessor :name
+  attr_reader :items
+
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+
+  
+end
