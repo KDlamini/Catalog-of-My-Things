72937251023e57fca8e65c274d1c5b2b51c38f62
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-09 10:12:49 +0200

    Revert "List options"

--- list_items.rb
+++ list_items.rb
@@ -2,14 +2,13 @@ require './book'
 
 module ListItems
   def list_labels
-    puts 'List of all labels : '
     @labels.each_with_index { |label, index| puts "#{index}) Title: #{label.title} | Color: #{label.color}" }
   end
 
   def list_all_books
-    puts 'List of all books : '
+    puts 'List of all Books : '
     if @books.empty?
-      puts "\nBooks list is empty."
+      puts "\n Books list is empty."
     else
       @books.each_with_index do |book, index|
         puts "#{index}) Title: #{book.label.title} | Publisher: #{book.publisher} | Condition: #{book.cover_state}"
