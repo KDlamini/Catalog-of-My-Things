da1e65ef06556d6fb7b6f9736a1d7ddb1130e916
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-09 22:25:17 +0200

    Add save_label method that saves new label to storage

--- modules/preserver_module.rb
+++ modules/preserver_module.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 module PreserverModule
   def create_file(path)
     Dir.mkdir('json') unless Dir.exist?('json')
@@ -36,13 +38,22 @@ module PreserverModule
     path = 'json/books.json'
     data = fetch_data(path)
 
-    new_book = { 
-        publisher: book.publisher,
-        cover_state: book.cover_state,
-        publish_date: book.publish_date,
+    new_book = {
+      publisher: book.publisher,
+      cover_state: book.cover_state,
+      publish_date: book.publish_date
     }
 
     data.push(new_book)
+    save(path, data)
+  end
+
+  def save_label(label)
+    path = 'json/labels.json'
+    data = fetch_data(path)
+    new_label = { title: label.title, color: book.color }
+
+    data.push(new_label)
     save(path, data)
   end
 end
