5fad0835182095406ad14477ac1e23b38fc27e55
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-09 19:33:08 +0200

    Add helper methods create_file,fetch_data,save, and file_exist? in PreserverModule

--- modules/preserver_module.rb
+++ modules/preserver_module.rb
@@ -1,3 +1,21 @@
 module PreserverModule
-    
+  def create_file(path)
+    Dir.mkdir('json') unless Dir.exist?('json')
+
+    File.open(path, 'w') do |file|
+      file.puts JSON.generate([])
+    end
+  end
+
+  def fetch_data(path)
+    JSON.parse(File.read(path), create_additions: true)
+  end
+
+  def save(path, data)
+    File.write(path, JSON.generate(data, create_additions: true))
+  end
+
+  def file_exist?(path)
+    File.exist? path
+  end
 end
