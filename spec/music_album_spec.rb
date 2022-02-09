ee6e0a486e5c92d3e9348c84fe6ea44f0840bd42
mbdesigns1989 <mbdesigns1989@yahoo.com>
2022-02-09 11:51:04 +0200

    Add unit tests for music_album

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,29 @@
+require 'date'
+require_relative '../classes/music_album'
+
+describe MusicAlbum do
+  context 'MusicAlbum class Test' do
+    on_spotify = true
+    name = '29'
+    publish_date = Date.parse('2021/05/02')
+    album = MusicAlbum.new(on_spotify, name, publish_date)
+
+    it 'is an instance of class item' do
+      expect(album).to be_an_instance_of(MusicAlbum)
+    end
+
+    it 'has on_spotify as true' do
+      expect(album.on_spotify).to eq(true)
+    end
+
+    it 'checks can_be_archived as false' do
+      expect(album.can_be_archived?).to eq(false)
+    end
+
+    new_date =  
+
+    it 'checks can_be_archived as true' do
+      expect(new_album.can_be_archived?).to eq(true)
+    end
+  end
+end
