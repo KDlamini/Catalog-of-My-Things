e0533e39919046d245a544146f170ea3514e6b7f
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-09 22:02:29 +0200

    Add add_label module that creates new label

---
+++ modules/add_label.rb
@@ -0,0 +1,30 @@
+module CreateLabel
+    def add_label
+        print "\nEnter label title: "
+        title = gets.chomp
+    
+        print 'Enter label color: '
+        color = gets.chomp.downcase
+
+        new_label = Label.new(title, color)
+
+        update_labels(new_label)
+        new_label
+    end
+
+    def validate_label_selection(index)
+        if index == @labels.size
+            add_label
+        elsif @labels[index]
+            @labels[index]
+        else
+            clear
+            puts "Invalid label selection. Please try again.\n\n"
+            puts "Select a book label by number/add new label.\n\n"
+            list_labels
+            puts "#{@labels.size}) Add new label"
+            label_index = gets.chomp.to_i
+            validate_label_selection(label_index)
+        end
+    end
+end
